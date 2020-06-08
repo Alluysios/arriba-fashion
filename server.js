dc93e8a9273ac2f82efd41636ce9e188fdba495c
Alluysios <arribaallu@gmail.com>
2020-06-08 12:34:13 -0600

    updated

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
 const app = require('./app');
 
-const db = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+const db = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 mongoose.connect(db,
     {
