240141930bc52f54544b53defba3922e29287a4f
Alluysios <arribaallu@gmail.com>
2020-06-08 12:38:30 -0600

    updated server

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
 const app = require('./app');
 
-const db = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-
-mongoose.connect(db,
+mongoose.connect(process.env.DATABASE_ARRIBA,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
